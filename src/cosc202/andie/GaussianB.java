15ff5fb684ae3793bb2fb6d100bba8095ce63ece
Jasper Hawkes <hawja651@student.otago.ac.nz>
2023-04-03 15:27:25 +1200

    Fixed GaussianB error handling

--- src/cosc202/andie/GaussianB.java
+++ src/cosc202/andie/GaussianB.java
@@ -13,9 +13,7 @@ public class GaussianB implements ImageOperation, java.io.Serializable {
         if (kernelSize < 0) {
             throw new IllegalArgumentException("Kernel Size cannot be negative.");
         }
-        if (sigma< 0) {
-            throw new IllegalArgumentException("Kernel Size cannot be negative.");
-        }
+        
         this.kernelSize = kernelSize;
         this.sigma = sigma;
     }
