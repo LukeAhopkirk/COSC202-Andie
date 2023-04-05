ad49a0045d4ef610891572f584d67d58cf24e762
Jasper Hawkes <hawja651@student.otago.ac.nz>
2023-04-05 18:15:41 +1200

    Flip Horizontal and Flip Vertical Javadoc comments

--- src/cosc202/andie/FlipH.java
+++ src/cosc202/andie/FlipH.java
@@ -1,30 +1,50 @@
-//COMPLETED Flipping image horizontally
-
 package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * <p>
+ * ImageOperation to flip an image horizontally.
+ * </p>
+ * 
+ */
 public class FlipH implements ImageOperation {
 
-    FlipH(){}
-    //Constructor
+    /**
+     * <p>
+     * Default Constructor
+     * </p>
+     */
+    FlipH() {
+    }
 
-    public BufferedImage apply(BufferedImage input){
+    /**
+     * <p>
+     * Apply a horizontal flip to an image.
+     * </p>
+     * 
+     * <p>
+     * Uses a nested loop iterating over the pixels of an image.
+     * Horizontally (from left to right) and vertically (from top to bottom).
+     * For each pixel, the code swaps its color value with the color value of the
+     * pixel on the opposite side of the image along the horizontal axis.
+     * </p>
+     * 
+     * @param input The image to flip horizontally.
+     * @return The resulting (flipped) image
+     */
+    public BufferedImage apply(BufferedImage input) {
 
         for (int y = 0; y < input.getHeight(); ++y) {
-            for (int x = 0; x  <= (input.getWidth()/2); ++x) {
+            for (int x = 0; x <= (input.getWidth() / 2); ++x) {
                 int firstPix = input.getRGB(x, y);
                 int secondPix = input.getRGB(input.getWidth() - x - 1, y);
-                
+
                 input.setRGB(input.getWidth() - x - 1, y, firstPix);
                 input.setRGB(x, y, secondPix);
             }
         }
-
         return input;
-
     }
-
-
 
 }
--- src/cosc202/andie/FlipV.java
+++ src/cosc202/andie/FlipV.java
@@ -1,30 +1,50 @@
-//COMPLETED Flipping image vertically
-
 package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * <p>
+ * ImageOperation to flip an image vertically.
+ * </p>
+ * 
+ */
 public class FlipV implements ImageOperation {
 
-    FlipV(){}
-    //Constructor
+    /**
+     * <p>
+     * Default Constructor
+     * </p>
+     */
+    FlipV() {
+    }
 
-    public BufferedImage apply(BufferedImage input){
+    /**
+     * <p>
+     * Apply a vertical flip to an image.
+     * </p>
+     * 
+     * <p>
+     * Uses a nested loop iterating over the pixels of an image.
+     * Horizontally (from left to right) and vertically (from top to bottom).
+     * For each pixel, the code swaps its color value with the color value of the
+     * pixel on the opposite side of the image along the vertical axis.
+     * </p>
+     * 
+     * @param input The image to flip vertically.
+     * @return The resulting (flipped) image
+     */
+    public BufferedImage apply(BufferedImage input) {
 
-        for (int y = 0; y <= (input.getHeight()/2); ++y) {
+        for (int y = 0; y <= (input.getHeight() / 2); ++y) {
             for (int x = 0; x < input.getWidth(); ++x) {
                 int firstPix = input.getRGB(x, y);
                 int secondPix = input.getRGB(x, input.getHeight() - y - 1);
-                
+
                 input.setRGB(x, input.getHeight() - y - 1, firstPix);
                 input.setRGB(x, y, secondPix);
             }
         }
-
         return input;
-
     }
-
-
 
 }
