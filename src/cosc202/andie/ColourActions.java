d3caec896ec02446a52c353b9b157d93e654e0b4
Jasper Hawkes <hawja651@student.otago.ac.nz>
2023-04-05 18:58:31 +1200

    Resize JavaDoc comments

--- src/cosc202/andie/ColourActions.java
+++ src/cosc202/andie/ColourActions.java
@@ -10,20 +10,23 @@ import javax.swing.*;
  * </p>
  * 
  * <p>
- * The Colour menu contains actions that affect the colour of each pixel directly 
+ * The Colour menu contains actions that affect the colour of each pixel
+ * directly
  * without reference to the rest of the image.
- * This includes conversion to greyscale in the sample code, but more operations will need to be added.
+ * This includes conversion to greyscale in the sample code, but more operations
+ * will need to be added.
  * </p>
  * 
- * <p> 
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * <p>
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
+ * 4.0</a>
  * </p>
  * 
  * @author Steven Mills
  * @version 1.0
  */
 public class ColourActions {
-    
+
     /** A list of actions for the Colour menu. */
     protected ArrayList<Action> actions;
     ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle");
@@ -35,9 +38,12 @@ public class ColourActions {
      */
     public ColourActions() {
         actions = new ArrayList<Action>();
-        actions.add(new ConvertToGreyAction(bundle.getString("Greyscale"), null, bundle.getString("GreyscaleDesc"), Integer.valueOf(KeyEvent.VK_G)));
-        actions.add(new BrightnessAction(bundle.getString("Brightness"), null, bundle.getString("BrightnessDesc"), Integer.valueOf(KeyEvent.VK_B)));
-        actions.add(new ContrastAction(bundle.getString("Contrast"), null, bundle.getString("ContrastDesc"), Integer.valueOf(KeyEvent.VK_C)));
+        actions.add(new ConvertToGreyAction(bundle.getString("Greyscale"), null, bundle.getString("GreyscaleDesc"),
+                Integer.valueOf(KeyEvent.VK_G)));
+        actions.add(new BrightnessAction(bundle.getString("Brightness"), null, bundle.getString("BrightnessDesc"),
+                Integer.valueOf(KeyEvent.VK_B)));
+        actions.add(new ContrastAction(bundle.getString("Contrast"), null, bundle.getString("ContrastDesc"),
+                Integer.valueOf(KeyEvent.VK_C)));
     }
 
     /**
@@ -50,7 +56,7 @@ public class ColourActions {
     public JMenu createMenu() {
         JMenu fileMenu = new JMenu(bundle.getString("Colour"));
 
-        for(Action action: actions) {
+        for (Action action : actions) {
             fileMenu.add(new JMenuItem(action));
         }
 
@@ -71,10 +77,10 @@ public class ColourActions {
          * Create a new convert-to-grey action.
          * </p>
          * 
-         * @param name The name of the action (ignored if null).
-         * @param icon An icon to use to represent the action (ignored if null).
-         * @param desc A brief description of the action  (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
          */
         ConvertToGreyAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
@@ -100,16 +106,44 @@ public class ColourActions {
 
     }
 
+    /**
+     * <p>
+     * Action to change Brightness of an image
+     * </p>
+     * 
+     * @see Brightness
+     */
     public class BrightnessAction extends ImageAction {
-        
-        //Calling constructor
+
+        /**
+         * <p>
+         * Create a new brightness action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
         BrightnessAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
+        /**
+         * <p>
+         * Callback for when the brightness action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the BrightnessAction is triggered.
+         * It changes the brightness of an image.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
         public void actionPerformed(ActionEvent e) {
             int brightness = 1;
-            
+
             // Pop-up dialog box to ask for the brightness value using a JSlider.
             JSlider brightnessSlider = new JSlider(-100, 100, 1);
             brightnessSlider.setMajorTickSpacing(50);
@@ -117,15 +151,16 @@ public class ColourActions {
             brightnessSlider.setPaintTicks(true);
             brightnessSlider.setPaintLabels(true);
             brightnessSlider.setSnapToTicks(true);
-            int option = JOptionPane.showOptionDialog(null, brightnessSlider, bundle.getString("BrightnessValueDesc"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
+            int option = JOptionPane.showOptionDialog(null, brightnessSlider, bundle.getString("BrightnessValueDesc"),
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
             // Check the return value from the dialog box.
             if (option == JOptionPane.CANCEL_OPTION) {
                 return;
             } else if (option == JOptionPane.OK_OPTION) {
                 brightness = brightnessSlider.getValue();
             }
-            
+
             // Create and apply the filter
             target.getImage().apply(new Brightness(brightness));
             target.repaint();
@@ -133,12 +168,41 @@ public class ColourActions {
         }
     }
 
+    /**
+     * <p>
+     * Action to change Contrast of an image
+     * </p>
+     * 
+     * @see Contrast
+     */
     public class ContrastAction extends ImageAction {
 
+        /**
+         * <p>
+         * Create a new contrast action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
         ContrastAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
 
+        /**
+         * <p>
+         * Callback for when the contrast action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the ContrastAction is triggered.
+         * It changes the contrast of an image.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
         public void actionPerformed(ActionEvent e) {
             int contrast = 1;
 
@@ -149,15 +213,16 @@ public class ColourActions {
             contrastSlider.setPaintTicks(true);
             contrastSlider.setPaintLabels(true);
             contrastSlider.setSnapToTicks(true);
-            int option = JOptionPane.showOptionDialog(null, contrastSlider, bundle.getString("ContrastValueDesc"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
+            int option = JOptionPane.showOptionDialog(null, contrastSlider, bundle.getString("ContrastValueDesc"),
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
             // Check the return value from the dialog box.
             if (option == JOptionPane.CANCEL_OPTION) {
                 return;
             } else if (option == JOptionPane.OK_OPTION) {
                 contrast = contrastSlider.getValue();
             }
-            
+
             // Create and apply the filter
             target.getImage().apply(new Brightness(contrast));
             target.repaint();
--- src/cosc202/andie/Resize.java
+++ src/cosc202/andie/Resize.java
@@ -3,39 +3,78 @@ package cosc202.andie;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+/**
+ * <p>
+ * ImageOperation to Resize an image
+ * </p>
+ * 
+ * <p>
+ * Changes the height and width of an image
+ * to a certain degree
+ * </p>
+ * 
+ */
 public class Resize implements ImageOperation {
+
     private double multiplier = 100;
 
-    Resize(){}
-    //Constructor
+    /**
+     * <p>
+     * Default Constructor
+     * </p>
+     */
+    Resize() {
+    }
 
-    Resize(double multiplier){
+    /**
+     * <p>
+     * Resize image with given multiplier
+     * </p>
+     * 
+     * <p>
+     * Multiplier is the percentage Resize applied.
+     * </p>
+     * 
+     * @param multiplier Percentage of current image that will be the new size
+     *                   e.g. 10% will produce an image 10% of the size
+     */
+    Resize(double multiplier) {
         if (multiplier < 0) {
             throw new IllegalArgumentException("Multiplier cannot be negative.");
         }
         this.multiplier = multiplier;
     }
 
-    //Method to resize an image based on new width and new height
-    public BufferedImage apply(BufferedImage input){
+    /**
+     * <p>
+     * Apply Resize to an image.
+     * </p>
+     * 
+     * <p>
+     * Alters the images height and width by finding
+     * current dimensions with .getHeight() & getWidth(),
+     * to then create a new BufferedImage with resized dimensions
+     * </p>
+     * 
+     * @param input The image to apply the Resize to.
+     * @return The resulting (resized) image.
+     */
+    public BufferedImage apply(BufferedImage input) {
 
         BufferedImage changeBuff = new BufferedImage(input.getColorModel(),
-            input.copyData(null),
-            input.isAlphaPremultiplied(), null);
+                input.copyData(null),
+                input.isAlphaPremultiplied(), null);
 
-        int newW = (int) (changeBuff.getWidth()*(multiplier/100));
-        int newH = (int) (changeBuff.getHeight()*(multiplier/100));
+        int newW = (int) (changeBuff.getWidth() * (multiplier / 100));
+        int newH = (int) (changeBuff.getHeight() * (multiplier / 100));
 
         // Convert Image to BufferedImage
         BufferedImage output = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-        
+
         Graphics g = output.createGraphics();
         g.drawImage(changeBuff, 0, 0, newW, newH, null);
         g.dispose();
-        
-        
 
         return output;
-
     }
 }
