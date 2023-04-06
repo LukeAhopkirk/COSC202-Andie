04d377f95ac47c2c2abf4b8ddc2d75fbe3c1b9d4
hoplu238 <hoplu238@student.otago.ac.nz>
2023-04-06 15:27:42 +1200

    Added comments to Rotate functions

--- src/cosc202/andie/Rotate90Right.java
+++ src/cosc202/andie/Rotate90Right.java
@@ -1,15 +1,39 @@
 package cosc202.andie;
 
 import java.awt.image.BufferedImage;
-
+/**
+ * <p>
+ * ImageOperation to rotate an image 90 degrees to tbe right.
+ * </p>
+ * 
+ */
 public class Rotate90Right implements ImageOperation{
+    /** 
+    * <p>
+    * Default Constructor
+    * </p>
+    */
     Rotate90Right(){
-        //Default contructor
     }
 
 
+    /**
+     * <p>
+     * Applys a rotation of 90 degrees to the image.
+     * </p>
+     * 
+     * <p>
+     * It makes a new image with the height of the old width and the width of 
+     * old height. It then Uses a nested for loop iterating over the pixels of an image.
+     * Horizontally (from left to right) and vertically (from top to bottom).
+     * From the top left, the code swaps its color value with the color value of the
+     * pixel starting from the top right of the new image.
+     * </p>
+     * 
+     * @param input The image to flip vertically.
+     * @return The resulting (flipped) image
+     */
     public BufferedImage apply(BufferedImage input){
-        //The following code  makes a new buffered image to the new size of the image
             
        BufferedImage inputImage = new BufferedImage(input.getColorModel(),input.copyData(null),
        input.isAlphaPremultiplied(), null);
